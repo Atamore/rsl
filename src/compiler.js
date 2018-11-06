ba1281900d31f3c8ca511c7155b9aef886e5b243
Anton Ignatev <nslsmain@gmail.com>
2018-11-06 04:52:10 +0200

    Remove unnecessary constant

--- src/compiler.js
+++ src/compiler.js
@@ -43,7 +43,6 @@ class Compiler {
             ],
             output: {
                 ...(configs.client.output || {}),
-                filename: constants.CLIENT_BUNDLE_NAME,
                 path: path.join(
                     process.cwd(),
                     constants.BUILD_PATH,
--- src/constants.js
+++ src/constants.js
@@ -4,7 +4,6 @@ module.exports = {
     SERVER_BUILD_FOLDER: 'server',
     CLIENT_PUBLIC_PATH: '/build/client/',
 
-    CLIENT_BUNDLE_NAME: 'client.js',
     SERVER_BUNDLE_NAME: 'server.js',
     CHUNKS_FILE_NAME: 'chunks.json'
 };
